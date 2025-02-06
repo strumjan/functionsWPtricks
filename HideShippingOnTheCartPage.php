e0239e79a943fb40bcdbc12bc4ca48fc8548e5f7
Ilija Iliev <strumjan@gmail.com>
2025-02-06 07:00:02 +0100

    Update HideShippingOnTheCartPage.php

--- HideShippingOnTheCartPage.php
+++ HideShippingOnTheCartPage.php
@@ -1,1 +1,9 @@
-Hide Shipping on the Cart page
+//Hide Shipping on the Cart page
+add_filter( 'woocommerce_cart_needs_shipping', 'filter_cart_needs_shipping' );
+function filter_cart_needs_shipping( $needs_shipping ) {
+    if ( is_cart() ) {
+        $needs_shipping = false;
+		echo "<style>tr.cart-subtotal {display: none !important;}</style>";
+    }
+    return $needs_shipping;
+}
